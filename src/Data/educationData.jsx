33f65c39bdd3ea49e65e798f803f24e7adb1d871
kaif7860123 <kaif47320@gmail.com>
2025-01-13 22:16:30 +0530

    educationData complete

---
+++ src/Data/educationData.jsx
@@ -0,0 +1,46 @@
+export const eduData=[
+    {
+        id:1,
+        techName:"B.Tech (Information Technology)",
+        universityName:" A.K.T.U university (2021-2025)",
+        marks:"8.2/10",
+        eduAbout:"The education should be very interactual. Ut tincidunt est acdolor aliquam sodales. Phasellus sed mauris hendrerit, laoreetsem in, lobortis mauris hendrerit ante."
+    },
+    {
+        id:2,
+        techName:"Intermediate (MATH)",
+        universityName:" U.P Board (2019-2021)",
+        marks:"84.6%",
+        eduAbout:"The education should be very interactual. Ut tincidunt est acdolor aliquam sodales. Phasellus sed mauris hendrerit, laoreetsem in, lobortis mauris hendrerit ante."
+    },
+    {
+        id:3,
+        techName:"High School (Science Side)",
+        universityName:" U.P Board (2019-2021)",
+
+        marks:"86.83%",
+        eduAbout:"The education should be very interactual. Ut tincidunt est acdolor aliquam sodales. Phasellus sed mauris hendrerit, laoreetsem in, lobortis mauris hendrerit ante."
+    },
+    {
+        id:4,
+        techName:"ZN Infotech (Fullstack)",
+        universityName:" A.K.T.U university (2021-2025)",
+        marks:"8.2/10",
+        eduAbout:"The education should be very interactual. Ut tincidunt est acdolor aliquam sodales. Phasellus sed mauris hendrerit, laoreetsem in, lobortis mauris hendrerit ante."
+    },
+    {
+        id:5,
+        techName:"Softflew Smart Solution",
+        universityName:" U.P Board (2019-2021)",
+        marks:"84.6%",
+        eduAbout:"The education should be very interactual. Ut tincidunt est acdolor aliquam sodales. Phasellus sed mauris hendrerit, laoreetsem in, lobortis mauris hendrerit ante."
+    },
+    {
+        id:6,
+        techName:"High School (Science Side)",
+        universityName:" U.P Board (2019-2021)",
+
+        marks:"86.83%",
+        eduAbout:"The education should be very interactual. Ut tincidunt est acdolor aliquam sodales. Phasellus sed mauris hendrerit, laoreetsem in, lobortis mauris hendrerit ante."
+    },
+]
