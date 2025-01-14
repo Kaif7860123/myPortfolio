8d2015624f8ce816b9c4b12645b02bcb6be5c8d2
kaif7860123 <kaif47320@gmail.com>
2025-01-14 13:41:43 +0530

    footer complete

---
+++ src/Pages/Footer/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './index.module.css'
+import logo from '../../assets/logo.png'
+function Footer() {
+  return (
+    <>
+     <div className={styles.footer}>
+        <div className={styles.imgDiv}>
+            <img src={logo}/>
+        </div>
+        <p className={styles.name}>Kaif</p>
+        <p className={styles.copyRight}>© 2025. All rights reserved by <span>Mohammad Kaif</span> </p>
+     </div>
+    </>
+  )
+}
+
+export default Footer
